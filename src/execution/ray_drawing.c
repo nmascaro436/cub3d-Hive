62cbb0195dd7acc223922390a7560631651ca3d1
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2025-12-09 12:18:30 +0200

    feat: added some comments

--- src/execution/ray_drawing.c
+++ src/execution/ray_drawing.c
@@ -6,7 +6,7 @@
 /*   By: nmascaro <nmascaro@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 12:18:41 by nmascaro          #+#    #+#             */
-/*   Updated: 2025/12/08 14:44:00 by nmascaro         ###   ########.fr       */
+/*   Updated: 2025/12/09 12:06:56 by nmascaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,61 +15,60 @@
 static mlx_texture_t *texture_selection(t_ray *ray, t_map *map)
 {
 	if (ray->wall.texture == NORTH_TEXT)
-		return (game->map->north_tex);
+		return (map->north_tex);
 	else if (ray->wall.texture == SOUTH_TEXT)
-		return (game->map->south_tex);
+		return (map->south_tex);
 	else if (ray->wall.texture == EAST_TEXT)
-		return(game->map->east_tex);
+		return (map->east_tex);
 	else
-		return(game->map->west_tex);
+		return (map->west_tex);
 }
 
-static int calculate_texture_x(t_ray *ray, mlx_texture_t *tex, t_player *p)
+static int get_texture_column(t_ray *ray, mlx_texture_t *tex, t_player *p)
 {
-	double wall_x;
+	double hit_pos; // where in the horizontal or vertical line of the wall we hit
 	int tex_x;
 
-	if (ray->dir.side == 0) //vertical wall
-        wall_x = player->y + ray->wall.perp_dist * ray->dir.dir_y;
-    else
-        wall_x = player->x + ray->wall.perp_dist * ray->dir.dir_x;
-    wall_x -= (int)wall_x; 
-    tex_x = (int)(wall_x * tex->width);
-    if (tex_x < 0)
+	if (ray->dir.side == 0) //vertical side of wall
+        hit_pos = p->y + ray->wall.perp_dist * ray->dir.dir_y; // x position of wall is fixed, we want to know where in y we hit (position of player + distance from player to wall * how much i move vertically)
+    else // horizontal side of wall
+        hit_pos = p->x + ray->wall.perp_dist * ray->dir.dir_x; // y position is fixed, we want to know where in x
+    hit_pos -= (int)hit_pos; // keep only decimal part (exactly where inside that tile we hit), we dont care here in what tile we are (same texture in the whole tile, different pixels though)
+    tex_x = (int)(hit_pos * tex->width); // which column of the texture does that hit correspond to
+    if (tex_x < 0) // safety check for going out of bounds
 		tex_x = 0;
     if (tex_x >= (int)tex->width)
 		tex_x = tex->width - 1;
     return (tex_x);
 }
 
-static int calculate_texture_y(int y, t_ray *ray, mlx_texture_t *tex)
+static int get_texture_row(int y, t_ray *ray, mlx_texture_t *tex)
 {
 	double tex_pos;
     int tex_y;
-	tex_pos = (double)(y - ray->wall.start_draw) / ray->wall.line_height;
-	tex_y = (int)(tex_pos * tex->height);
-    if (tex_y < 0)
+	tex_pos = (double)(y - ray->wall.start_draw) / (double)ray->wall.line_height; // where in the column
+	tex_y = (int)(tex_pos * tex->height); // which row of the texture 
+    if (tex_y < 0) //safety check for out of bounds
 		tex_y = 0;
     if (tex_y >= (int)tex->height)
 		tex_y = tex->height - 1;
     return (tex_y);
 }
 
-static int get_pixel_texture(mlx_texture_t *tex, int tex_x, int tex_y)
+static int get_texture_color(mlx_texture_t *tex, int tex_x, int tex_y)
 {
-    int index;
+    int pixel_i;
 	uint8_t r;
 	uint8_t g;
 	uint8_t b;
 	uint8_t a;
 
-	index = (tex_y * tex->width + tex_x) * tex->bytes_per_pixel;
-
-    r = tex->pixels[index + 0];
-    g = tex->pixels[index + 1];
-    b = tex->pixels[index + 2];
+	pixel_i = (tex_y * tex->width + tex_x) * tex->bytes_per_pixel;
+    r = tex->pixels[pixel_i + 0];
+    g = tex->pixels[pixel_i + 1];
+    b = tex->pixels[pixel_i + 2];
     if (tex->bytes_per_pixel == 4)
-        a = tex->pixels[index + 3];
+        a = tex->pixels[pixel_i + 3];
     else
         a = 255;
     return ((r << 24) | (g << 16) | (b << 8) | a);
@@ -81,14 +80,14 @@ void draw_ray(t_ray *ray, t_game *game, int x)
 	int tex_y;
 	int y;
 	uint32_t color;
-
+	
 	tex = texture_selection(ray, game->map);
-	tex_x = calculate_texture_x(ray, tex, game->player);
+	tex_x = get_texture_column(ray, tex, game->player);
 	y = ray->wall.start_draw;
 	while (y <= ray->wall.end_draw)
 	{
-		tex_y = calculate_texture_y(y, ray, tex);
-		color = get_pixel_texture(tex, tex_x, tex_y);
+		tex_y = get_texture_row(y, ray, tex); // each row in that column
+		color = get_texture_color(tex, tex_x, tex_y);
 		mlx_put_pixel(game->img, x, y, color);
 		y++;
 	}
